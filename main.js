49c3f14d36c7d51f0c290a4c00784e891c8aae26
ludfjig <ludvil@uw.edu>
2019-04-30 14:25:49 -0700

    fixed style

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// Name: _your name here_
-// Date: _add date here_
-// Section: CSE 154 _your section here_
+// Name: Ludvig Liljenberg
+// Date: 03/30/2019
+// Section: CSE 154AF
 //
-// -- your description of what this file does here --
-// Do not keep comments from this template in any work you submit (functions included under "Helper
-// functions" are an exception, you may keep the function names/comments of checkStatus/id/qs/qsa)
+// This script makes use of the fetch api to query The Movie Database for movies
+// using a string entered by the user. The resulting movies are displayed to the user.
+//
 
 (function() {
   "use strict";
-
-
+  const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+  const API_KEY = "fc6ca6162c5996649327d4c5d3e46554";
+  const POSTER_URL = "https://image.tmdb.org/t/p/original";
 
   /**
    *  Add a function that will be called when the window is loaded.
@@ -17,71 +18,79 @@
   window.addEventListener("load", init);
 
   /**
-   *  CHANGE: Describe what your init function does here.
+   *  Adds responsiveness to the search button
    */
   function init() {
     qs("button").addEventListener("click", search);
   }
 
+  /**
+   * Retrieves information from The Movie Database's api and displays it to the user.
+   * Removes all previously displayed movies.
+   */
   function search() {
+    //removes old movies
     let section = qs("section");
     while(section.firstChild){
       section.firstChild.remove();
     }
-    let title = qs("input").value;
-    let url = "https://api.themoviedb.org/3/search/movie?api_key=fc6ca6162c5996649327d4c5d3e46554&query=";
-    url += title;
-    fetch(url).then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        let j = myJson.results;
-        for(let i = 0; i < myJson.results.length; i++){
-          let movie = document.createElement("div");
-          let title = document.createElement("h3");
-          let poster = document.createElement("img");
-          let desc = document.createElement("p");
 
-          title.innerText = j[i].title;
-          desc.innerText = j[i].overview;
-          poster.src = "https://image.tmdb.org/t/p/original"+j[i].poster_path;
-
-          movie.appendChild(poster);
-          movie.appendChild(title);
-          movie.appendChild(desc);
-          qs("section").appendChild(movie);
-        }
-      })
-      .catch(function (err){
-        console.log("error");
-        // fix this
-      });
-  }
-
-
-  /**
-   *  Make sure to always add a descriptive comment above
-   *  every function detailing what it's purpose is
-   *  Use JSDoc format with @param and @return.
-   */
-  function exampleFunction1() {
-    /* SOME CODE */
+    //creates new ones
+    let url = BASE_URL + "?api_key=" + API_KEY;
+    url += "&query=" + qs("input").value;
+    fetch(url)
+      .then(checkStatus)
+      .then(JSON.parse)
+      .then(createMovieCards);
+      // .catch(function (err){
+      //   console.log("error");
+      //   // fix this
+      // });
   }
 
   /**
-   *  Make sure to always add a descriptive comment above
-   *  every function detailing what it's purpose is
-   *  @param {variabletype} someVariable This is a description of someVariable, including, perhaps, preconditions.
-   *  @returns A description of what this function is actually returning
+   * Creates a movie "card" html element for every movie in the given json object,
+   * and displays them to the user.
+   * @param  {JSON object} myJson - the json object returned from The Movie Database
    */
-  function exampleFunction2(someVariable) {
-    /* SOME CODE */
-    return something;
+  function createMovieCards(jsonResponse) {
+    let movies = jsonResponse.results;
+    for(let i = 0; i < movies.length; i++){
+      let movie = document.createElement("div");
+      let title = document.createElement("h3");
+      let poster = document.createElement("img");
+      let desc = document.createElement("p");
+
+      title.innerText = movies[i].title;
+      desc.innerText = movies[i].overview;
+      poster.src = POSTER_URL+movies[i].poster_path;
+
+      movie.appendChild(poster);
+      movie.appendChild(title);
+      movie.appendChild(desc);
+      qs("section").appendChild(movie);
+    }
+    console.log(movies.length);
   }
 
   /* ------------------------------ Helper Functions  ------------------------------ */
   // Note: You may use these in your code, but do remember that your code should not have
   // any functions defined that are unused.
+
+  /**
+     * Helper function to return the response's result text if successful, otherwise
+     * returns the rejected Promise result with an error status and corresponding text
+     * @param {object} response - response to check for success/error
+     * @returns {object} - valid result text if response was successful, otherwise rejected
+     *                     Promise result
+     */
+    function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return response.text();
+      } else {
+        return Promise.reject(new Error(response.status + ": " + response.statusText));
+      }
+    }
 
   /**
    * Returns the element that has the ID attribute with the specified value.
